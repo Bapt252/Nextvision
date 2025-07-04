e754e2840209f6edf0500807b966a33b8fd12b3b
Bapt252 <baptiste.coma@gmail.com>
2025-07-04 17:21:05 +0200

    🎯 Init package Nextvision pour intégration Bridge

---
+++ nextvision/__init__.py
@@ -0,0 +1,11 @@
+"""
+🎯 Nextvision - Package principal
+Algorithme de matching IA adaptatif pour NEXTEN
+
+Author: NEXTEN Team
+Version: 1.0.0
+"""
+
+__version__ = "1.0.0"
+__author__ = "NEXTEN Team"
+__description__ = "Algorithme de matching IA adaptatif avec pondération contextuelle"
