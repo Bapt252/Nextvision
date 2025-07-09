0d14ac8bb857b95c42e6f54855b1ee6bc39f0400
Bapt252 <baptiste.coma@gmail.com>
2025-07-09 10:27:02 +0200

    🔧 OPTION 1: Création package scorers_v3 avec __init__.py
    - Package Python pour scorers V3.0  
    - Import LocationTransportScorerV3
    - Version 3.0.0 Transport Intelligence Revolution

---
+++ nextvision/services/scorers_v3/__init__.py
@@ -0,0 +1,12 @@
+"""
+🚀 Nextvision V3.0 - Scorers V3 Package (PROMPT 5)
+Package des nouveaux scorers révolutionnés avec Transport Intelligence
+
+Author: NEXTEN Team
+Version: 3.0.0 - Transport Intelligence Revolution
+"""
+
+from .location_transport_scorer_v3 import LocationTransportScorerV3
+
+__version__ = "3.0.0"
+__all__ = ["LocationTransportScorerV3"]
